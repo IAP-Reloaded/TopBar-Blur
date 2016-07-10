22d7b758a8376a4e51fff24da460a9a6bc33bd94
IAP-Reloaded <IAP-Reloaded@users.noreply.github.com>
2016-07-10 17:16:40 -0400

    Create script.user.js

---
+++ script.user.js
@@ -0,0 +1,4 @@
+var style = document.createElement('style')
+style.type = 'text/css'
+style.innerHTML = '.topbar{background-color:rgba(12,13,14,.4)!important;-webkit-backdrop-filter:blur(10px)}.triangle,.topbar-icon:not(.icon-site-switcher){-webkit-filter:brightness(200%);filter:brightness(200%)}.topbar-icon.yes-hover:hover,.topbar-links a:hover,.topbar-icon.topbar-icon-on{background-color:rgba(59,64,69,.4)!important}'
+document.getElementsByTagName('head')[0].appendChild(style)
